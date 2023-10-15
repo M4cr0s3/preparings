f0083b8fc39f902bb451c1f73851867df9f8bc2e
M4cr0s3 <tmmllllllllll@gmail.com>
2023-10-15 20:49:04 +0300

    init

--- preparings/app/Http/Controllers/API/CabinetController.php
+++ preparings/app/Http/Controllers/API/CabinetController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CabinetCollection;
-use App\Http\Resources\CabinetResource;
 use App\Models\Cabinet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class CabinetController extends Controller
 {
-    public function getAllCabinets()
+    /**
+     * @return CabinetCollection
+     */
+    public function getAllCabinets(): CabinetCollection
     {
-        $cabinets = Cabinet::query()
-            ->with('corpse')
-            ->get();
-
-        return new CabinetCollection($cabinets);
+        return new CabinetCollection(Cache::remember('cabinets', 60 * 60 * 24, function () {
+            return Cabinet::query()->with('corpse')->get();
+        }));
     }
 }
